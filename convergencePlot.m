713422f53119997aba87722346f86e04ecc271de
Matthew-Saurette <matthews14@hotmail.ca>
2020-10-24 19:16:21 -0700

    added convergence plot code + a test example in test.m

--- convergencePlot.m
+++ convergencePlot.m
@@ -2,3 +2,28 @@
 
 %input: [functionevals , f(yk)] for each of the three problems
 %output: png of convergence plot
+
+classdef convergencePlot
+    methods(Static)
+        function j = plot(X,Y,Z,outputFile)
+            logplot = false
+            plot(X(:,1),X(:,2))
+            hold on
+            plot(Y(:,1),Y(:,2))
+            hold on
+            plot(Z(:,1),Z(:,2))
+            hold on
+            
+            legend('Problem 1','Problem 2','Problem 3');
+
+            if (logplot) 
+                xlabel(strcat('log_2(Number of Function Evaluations)')); 
+                ylabel('Best Function Value Found');
+            else
+                xlabel('Number of Function Evaluations');
+                ylabel('Best Function Value Found')
+            end
+            saveas(gcf,strcat(outputFile,'ConvergencePlot.png'));
+        end
+    end
+end
--- test.m
+++ test.m
@@ -10,3 +10,10 @@ Y=[0,-1,0,0;
    0,0,0,-1];
 
 simplexPlot.plot(X,Y,1,'C:\Users\User\Desktop\Math462\GroupProject\Cosc419KGroupProject\testSimplexPlotFolder\');
+
+
+%testing convergence plot%
+X =[0,100; 1,90;2,80;3,70];
+Y =[0,110; 1,100;2,90];
+Z =[0,70; 1,60;2,50];
+convergencePlot.plot(X,Y,Z,'C:\Users\User\Desktop\Math462\GroupProject\Cosc419KGroupProject\testConvergenceFolder\');
