5596c12974f9ac43c1622a2f8b95dcd6c5c31431
Tyler Weames <tweames@gmail.com>
2020-10-25 16:17:55 -0700

    Comments on the purpose of this m file

--- centroid_practice.m
+++ centroid_practice.m
@@ -1,9 +1,10 @@
+% Place to check vector calculations before pushing into main
+
 clc;
 clear all;
 
 Y = [0 0 0; 1 0 0; 0 1 0; 0 0 1];
-x_c = (1/rank(Y))*sum(Y(1:rank(Y),:))
+x_c = (1/rank(Y))*sum(Y(1:rank(Y),:));
 
-Y(rank(Y) + 1,:)
-x_r = x_c + (x_c - Y(rank(Y) + 1, :))
-f_r = 
+Y(rank(Y) + 1,:);
+x_r = x_c + (x_c - Y(rank(Y) + 1, :));
