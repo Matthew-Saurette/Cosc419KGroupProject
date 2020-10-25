6788e08e64a8d1f03eb6a63cb0391eb992dc52ed
Matthew-Saurette <matthews14@hotmail.ca>
2020-10-25 14:29:52 -0700

    added ability to plot all the Simplicies on 1 plot to show the progression (Might be usefull)

--- simplexPlot.m
+++ simplexPlot.m
@@ -12,11 +12,29 @@ classdef simplexPlot
 
             s1 = trimesh(k1,X(1,:),X(2,:),X(3,:),'FaceColor','cyan','DisplayName',strcat('Y',string(iter)));
             hold on;
-            s2 = trimesh(k2,Y(1,:),Y(2,:),Y(3,:),'FaceColor','cyan','DisplayName',strcat('Y',string(iter+1)));
+            s2 = trimesh(k2,Y(1,:),Y(2,:),Y(3,:),'FaceColor','blue','DisplayName',strcat('Y',string(iter+1)));
             alpha(s1,0.1);
             legend show;
             saveas(gcf,strcat(outputFile,'simplex',string(iter), '-',string(iter+1),'.png'));
             hold off;
+        end
+        
+        function j = plotTotal(T,outputFile)
+            
+            for i = 1:length(T)
+                X = T{i}
+                [k1,av1] = convhull(X(1,:),X(2,:),X(3,:));
+                s1 = trimesh(k1,X(1,:),X(2,:),X(3,:),'FaceColor',[1,i/length(T),0],'DisplayName',strcat('Y',string(i)));
+                alpha(s1,0.1);
+                if i ==1
+                    hold on;
+                end
+            end
+                
+            legend show;
+            saveas(gcf,strcat(outputFile,'simplexCombined','.png'));
+            hold off;
+            
         end
     end
 end
--- test.m
+++ test.m
@@ -8,10 +8,13 @@ X=[0,1,0,0;
 Y=[0,-1,0,0;
     0,0,-1,0;
    0,0,0,-1];
+Z = [0,-0.5,0,0;
+    0,0,-.5,0;
+   0,0,0,-.5];
 
 simplexPlot.plot(X,Y,1,'C:\Users\User\Desktop\Math462\GroupProject\Cosc419KGroupProject\testSimplexPlotFolder\');
-
-
+X = {X,Y,Z}
+simplexPlot.plotTotal(X,'C:\Users\User\Desktop\Math462\GroupProject\Cosc419KGroupProject\testSimplexPlotFolder\');
 %testing convergence plot%
 X =[0,100; 1,90;2,80;3,70];
 Y =[0,110; 1,100;2,90];
