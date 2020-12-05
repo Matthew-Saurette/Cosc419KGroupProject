eb48e3e75bcb333c3157f5465ac8e30caa29f28e
Tyler Weames <tweames@gmail.com>
2020-12-04 16:47:29 -0800

    some get pic edits

--- Image_editor.m
+++ Image_editor.m
@@ -3,11 +3,26 @@ clear all;
 clc;
 
 % Opens saved figure
-h = openfig('NiceFunctionSimplex.fig');
-hold on
+%% Edits axes
 
-xlabel('x');
-ylabel('y');
-zlabel('z');
+ h = openfig('niceFunctionSimplex.fig');
+ hold on
+% 
+% xlabel('x');
+% ylabel('y');
+% zlabel('z');
+% 
+% print(h_current, 'NiceFunctionSimplex_edit.jpg','-dpdf')
 
-print(h_current, 'NiceFunctionSimplex_edit.jpg','-dpdf')
+% This file will be used to open figures.
+clear all;
+clc;
+
+%% Trims the white space from the images for presentation
+
+h_current = figure(1);
+axes = gca;
+set(h_current, 'Units','Inches');
+pos = get(h_current,'Position');
+set(h_current,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3),pos(4)]);
+print(h_current, 'niceFunctionSimplex','-djpeg')
--- ToGetPicturesOfSimplexes.m
+++ ToGetPicturesOfSimplexes.m
@@ -1,10 +1,21 @@
 clc; close all; clear;
-Y0a = [1 0 0 0;
-       0 1 0 0;
-       0 0 1 0];
-% Y0a(:,:,1) = [9.5 10.5 9.5 9.5;
-%               9.5 9.5 10.5 9.5;
-%               9.5 9.5 9.5 10.5];
+Y0a(:,:,1) = [0 -10 -1 0;
+              0 5 -21 0;
+              0 7 -1 1];
+del_e = 2;            
+del_oc = 1/2;
+del_ic = -1/2;
+gamma = 1/2;
+solution = 0;
+eps = 1e-6;
+f = @(x) (1-x(1))^2 + 100*(x(2)-x(1))^2;
+
+[YkTotal, fkbest, feval_total] = nelderMead(Y0a,del_e,del_oc,del_ic,gamma,f, solution, eps);
+YkTotal(1,1,500)
+YkTotal(2,1,500)
+%  Y0a(:,:,1) = [9.5 10.5 9.5 9.5;
+%                9.5 9.5 10.5 9.5;
+%                9.5 9.5 9.5 10.5];
 
 %Y0a(:,4,2) = [1/3;1/3;0] + ([1/3;1/3;0]-Y0a(:,4,2))     %reflection
 %Y0a(:,4,2) = [1/3;1/3;0] + 2.*([1/3;1/3;0]-Y0a(:,4,2))     %expansion  
@@ -23,7 +34,7 @@ hold on
 %plot3(0.1667, 0.1667, 0.5, 'o', 'linewidth', 2, 'Color', 'r')      %IC
 %plot3(1, 0, 0, 'o', 'linewidth', 2, 'Color', 'r')      %Shrink - y0
 %plot3(9.5062, 8.4167, 8.7269, 'o', 'linewidth', 2, 'Color', 'r')
-plot3(0, 0, -2, 'o', 'linewidth', 2, 'Color', 'r')
+plot3(YkTotal(1,1,500), YkTotal(2,1,500), fkbest(500), 'o', 'linewidth', 2, 'Color', 'r')
 xlabel('x');
 ylabel('y');
 zlabel('z');
